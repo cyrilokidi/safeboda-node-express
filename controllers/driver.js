a4cd295396ed73f1fe9ccb48c1e142ccbaed866b
cyrilokidi <okidicyril@gmail.com>
2022-01-08 20:47:37 +0300

    update create driver response body

--- controllers/driver.js
+++ controllers/driver.js
@@ -1,12 +1,15 @@
 const Service = require('../services/driver');
+const validator = require('../middlewares/validator');
+const schema = require('../schemas/driver');
 
 module.exports = {
   create: [
+    validator(schema.create),
     async (req, res, next) => {
       try {
         const { body } = req;
         const service = new Service();
-        const driver = await service.create(body);
+        const [driver] = await service.create(body);
 
         res.status(201);
         res.json(driver);
