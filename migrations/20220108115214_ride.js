74539d30c5eff45c4bd306b95de45efcd387e6d0
cyrilokidi <okidicyril@gmail.com>
2022-01-08 15:08:41 +0300

    define table ride

---
+++ migrations/20220108115214_ride.js
@@ -0,0 +1,34 @@
+const name = 'ride';
+
+exports.up = (knex) =>
+  knex.schema.createTable(name, (table) => {
+    table.uuid('passenger_id').notNullable();
+    table.uuid('driver_id').notNullable();
+    table.decimal('pickup_point_lat', 8, 6);
+    table.decimal('pickup_point_long', 9, 6);
+    table.decimal('destination_lat', 8, 6);
+    table.decimal('destination_long', 9, 6);
+    table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
+
+    // index
+
+    table
+      .foreign('passenger_id')
+      .references('id')
+      .inTable('passenger')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+
+    table
+      .foreign('driver_id')
+      .references('id')
+      .inTable('driver')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+
+    // constraint
+
+    table.unique(['passenger_id', 'driver_id']);
+  });
+
+exports.down = (knex) => knex.schema.dropTable(name);
