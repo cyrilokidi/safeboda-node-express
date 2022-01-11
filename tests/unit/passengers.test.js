d9827e9f77a4820efef6bb3ece225b092c374d7e
cyrilokidi <okidicyril@gmail.com>
2022-01-11 10:24:09 +0300

    add create new passenger test case

--- tests/unit/passengers.test.js
+++ tests/unit/passengers.test.js
@@ -1,32 +1,32 @@
-const Service = require('../../services/driver');
+const Service = require('../../services/passenger');
 const fs = require('fs');
 const data = fs.readFileSync(__dirname + '/../data.json', 'utf-8');
-const { driver } = JSON.parse(data);
+const { passenger } = JSON.parse(data);
 const { expect } = require('chai');
 const { db } = require('../../util');
 
-describe('Driver management', function () {
+describe('Passenger management', function () {
   before(function (done) {
-    // Remove driver before test.
+    // Remove passenger before test
 
-    db('driver')
-      .where({ phone_number: driver.phone_number })
+    db('passenger')
+      .where({ phone_number: passenger.phone_number })
       .del()
       .then(() => done())
       .catch((err) => done(err));
   });
 
-  it('Should successfully create new driver', function (done) {
+  it('Should successfully create new passenger', function (done) {
     const service = new Service();
 
-    // Create new driver
+    // Create new passenger
     service
-      .create({ ...driver })
+      .create({ ...passenger })
       .then(([result]) => {
         expect(result).to.be.an('object');
         expect(result).to.have.property('id');
-        expect(result).to.have.property('name', driver.name);
-        expect(result).to.have.property('phone_number', driver.phone_number);
+        expect(result).to.have.property('name', passenger.name);
+        expect(result).to.have.property('phone_number', passenger.phone_number);
         expect(result).to.have.property('created_at');
 
         done();
