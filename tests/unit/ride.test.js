7ea09df042246e41f22505d6493e87b08f74b214
cyrilokidi <okidicyril@gmail.com>
2022-01-11 17:04:36 +0300

    add test suit to stop ongoing ride

--- tests/unit/ride.test.js
+++ tests/unit/ride.test.js
@@ -47,7 +47,7 @@ describe('Ride management', function () {
           expect(result).to.have.property('id');
           expect(result).to.have.property('passenger_id', passengerId);
           expect(result).to.have.property('driver_id', driverId);
-          expect(result).to.have.property('done', ride.done);
+          expect(result).to.have.property('done');
           expect(result).to.have.property(
             'pickup_point_lat',
             ride.pickup_point_lat
@@ -78,6 +78,116 @@ describe('Ride management', function () {
         .del()
         .then(() => db('driver').where({ id: driverId }).del())
         .then(() => done())
+        .catch((err) => done(err));
+    });
+  });
+
+  describe('Stop ongoing ride', function () {
+    let passengerId = null;
+    let driverId = null;
+    let rideId = null;
+
+    before(function (done) {
+      db.transaction(function (trx) {
+        db('passenger')
+          .where({ phone_number: passenger.phone_number })
+          .del()
+          .transacting(trx)
+          .then(() =>
+            db('passenger')
+              .insert({ ...passenger }, ['id'])
+              .transacting(trx)
+          )
+          .then(([response]) => {
+            passengerId = response.id;
+
+            return db('driver')
+              .where({ phone_number: driver.phone_number })
+              .del()
+              .transacting(trx);
+          })
+          .then(() =>
+            db('driver')
+              .insert({ ...driver }, ['id'])
+              .transacting(trx)
+          )
+          .then(([response]) => {
+            driverId = response.id;
+
+            return db('ride')
+              .where({ passenger_id: passengerId, driver_id: driverId })
+              .del()
+              .transacting(trx);
+          })
+          .then(() =>
+            db('ride')
+              .insert(
+                { passenger_id: passengerId, driver_id: driverId, ...ride },
+                ['id']
+              )
+              .transacting(trx)
+          )
+          .then(([response]) => {
+            rideId = response.id;
+
+            return;
+          })
+          .then(trx.commit)
+          .catch(trx.rollback);
+      })
+        .then(() => done())
+        .catch((err) => done(err));
+    });
+
+    after(function (done) {
+      db.transaction(function (trx) {
+        db('passenger')
+          .where({ id: passengerId })
+          .del()
+          .transacting(trx)
+          .then(() =>
+            db('driver').where({ id: driverId }).del().transacting(trx)
+          )
+          .then(() => db('ride').where({ id: rideId }).del().transacting(trx))
+          .then(trx.commit)
+          .catch(trx.rollback);
+      })
+        .then(() => done())
+        .catch((err) => done(err));
+    });
+
+    it('Should successfully stop an ongoing ride', function (done) {
+      const service = new Service();
+
+      service
+        .stop(rideId)
+        .then((response) => {
+          expect(response).to.be.an('array').length(1);
+          expect(response[0]).to.be.an('object');
+          expect(response[0]).to.have.property('id');
+          expect(response[0]).to.have.property('passenger_id', passengerId);
+          expect(response[0]).to.have.property('driver_id', driverId);
+          expect(response[0]).to.have.property('done');
+          expect(response[0]).to.have.property(
+            'pickup_point_lat',
+            ride.pickup_point_lat
+          );
+          expect(response[0]).to.have.property(
+            'pickup_point_long',
+            ride.pickup_point_long
+          );
+          expect(response[0]).to.have.property(
+            'destination_lat',
+            ride.destination_lat
+          );
+          expect(response[0]).to.have.property(
+            'destination_long',
+            ride.destination_long
+          );
+          expect(response[0]).to.have.property('created_at');
+
+          done();
+        })
         .catch((err) => done(err));
     });
   });
