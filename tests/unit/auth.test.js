c382a5f032ec8e30316973139a16deb1812019f5
cyrilokidi <okidicyril@gmail.com>
2022-01-11 04:34:04 +0300

    add test case for wrong auth credentials

--- tests/unit/auth.test.js
+++ tests/unit/auth.test.js
@@ -5,7 +5,7 @@ const Service = require('../../services/auth');
 const errors = require('../../errors');
 
 describe('Admin authentication', function () {
-  it('Should successfully login admin', function () {
+  it('Should successfully login admin', function (done) {
     const data = {
       email: process.env.ADMIN_EMAIL,
       password: process.env.ADMIN_PASSWORD,
@@ -14,5 +14,20 @@ describe('Admin authentication', function () {
     const test = service.login(data);
 
     expect(test).to.be.a('string');
+
+    done();
+  });
+
+  it('Should fail to login admin with wrong credentials', function (done) {
+    const data = {
+      email: process.env.ADMIN_PASSWORD,
+      password: process.env.ADMIN_EMAIL,
+    };
+    const service = new Service();
+    const test = service.login.bind(service, data);
+
+    expect(test).to.throw(errors.AuthenticationError);
+
+    done();
   });
 });
