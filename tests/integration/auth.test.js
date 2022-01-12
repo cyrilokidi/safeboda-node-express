80edbfa3373d39dcd029400bdc473140836f534a
cyrilokidi <okidicyril@gmail.com>
2022-01-12 06:09:46 +0300

    create login admin with wrong creds. test case

--- tests/integration/auth.test.js
+++ tests/integration/auth.test.js
@@ -6,7 +6,7 @@ const { expect } = chai;
 chai.use(chaiHttp).request(`/localhost:${process.env.PORT}`);
 
 describe('Admin authentication', function () {
-  it('Should successfully login user', function (done) {
+  it('Should successfully login admin', function (done) {
     chai
       .request(server)
       .post('/login')
@@ -19,6 +19,27 @@ describe('Admin authentication', function () {
         expect(response).to.be.status(200);
         expect(response.body).to.be.an('object');
         expect(response.body).to.have.property('token');
+
+        done();
+      });
+  });
+
+  it('Should fail to login admin with wrong credentials', function (done) {
+    chai
+      .request(server)
+      .post('/login')
+      .send({
+        email: process.env.ADMIN_EMAIL,
+        password: process.env.ADMIN_EMAIL,
+      })
+      .end((err, response) => {
+        expect(err).to.be.null;
+        expect(response).to.be.status(401);
+        expect(response.body).to.be.an('object');
+        expect(response.body).to.have.property('name');
+        expect(response.body).to.have.property('code');
+        expect(response.body).to.have.property('date');
+        expect(response.body).to.have.property('message');
 
         done();
       });
