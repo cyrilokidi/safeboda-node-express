de10720dc78b2f50c46de26660cc5d2d0f8941e6
cyrilokidi <okidicyril@gmail.com>
2022-01-13 16:54:34 +0300

    add test case to create new driver

--- tests/integration/driver.test.js
+++ tests/integration/driver.test.js
@@ -9,8 +9,8 @@ const { db } = require('../../util');
 
 chai.use(chaiHttp);
 
-describe('Driver management', function () {
-  let authorization = null;
+describe('Driver end-points', function () {
+  let authorization;
 
   before(function (done) {
     chai
@@ -20,90 +20,39 @@ describe('Driver management', function () {
         email: process.env.ADMIN_EMAIL,
         password: process.env.ADMIN_PASSWORD,
       })
-      .then((response) => {
-        authorization = `Bearer ${response.body.token}`;
+      .then((res) => {
+        authorization = `Bearer ${res.body.token}`;
 
         done();
       })
       .catch((err) => done(err));
   });
 
-  describe('Create new driver', function () {
-    let id = null;
-
-    after(function (done) {
-      db('driver')
-        .where({ id })
-        .del()
-        .then(() => done())
-        .catch((err) => done(err));
-    });
-
-    it('Should successfully create new driver', function (done) {
-      chai
-        .request(server)
-        .post('/driver')
-        .set('Authorization', authorization)
-        .send({ ...driver })
-        .then((res) => {
-          expect(res).to.be.status(201);
-          expect(res.body).to.be.an('object');
-          expect(res.body).to.have.property('id');
-          expect(res.body).to.have.property('name', driver.name);
-          expect(res.body).to.have.property(
-            'phone_number',
-            driver.phone_number
-          );
-          expect(res.body).to.have.property('suspended');
-          expect(res.body).to.have.property('created_at');
-
-          id = res.body.id; // for deleting driver after test
-
-          done();
-        })
-        .catch((err) => done(err));
-    });
+  afterEach(function (done) {
+    db('driver')
+      .where({ phone_number: driver.phone_number })
+      .del()
+      .then(() => done())
+      .catch((err) => done(err));
   });
 
-  describe('Create existing driver', function () {
-    let id = null;
+  it('Should successfully create new driver', function (done) {
+    chai
+      .request(server)
+      .post('/driver')
+      .set('Authorization', authorization)
+      .send({ ...driver })
+      .then((res) => {
+        expect(res).to.be.status(201);
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.have.property('id').that.is.a('string');
+        expect(res.body).to.have.property('name', driver.name);
+        expect(res.body).to.have.property('phone_number', driver.phone_number);
+        expect(res.body).to.have.property('suspended').that.is.a('boolean');
+        expect(res.body).to.have.property('created_at').that.is.a('string');
 
-    before(function (done) {
-      db('driver')
-        .insert({ ...driver }, ['id'])
-        .then(([res]) => {
-          id = res.id; // for deleting driver after test
-
-          done();
-        })
-        .catch((err) => done(err));
-    });
-
-    after(function (done) {
-      db('driver')
-        .where({ id })
-        .del()
-        .then(() => done())
-        .catch((err) => done(err));
-    });
-
-    it('Should fail to create existing driver', function (done) {
-      chai
-        .request(server)
-        .post('/driver')
-        .set('Authorization', authorization)
-        .send({ ...driver })
-        .then((res) => {
-          expect(res).to.have.status(409);
-          expect(res.body).to.be.an('object');
-          expect(res.body).to.have.property('name');
-          expect(res.body).to.have.property('code');
-          expect(res.body).to.have.property('date');
-          expect(res.body).to.have.property('message');
-
-          done();
-        })
-        .catch((err) => done(err));
-    });
+        done();
+      })
+      .catch((err) => done(err));
   });
 });
